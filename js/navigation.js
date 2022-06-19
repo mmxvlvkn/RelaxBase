e2d061c2ebb111c99e40b5093fd75147ebdee938
Valyavkin Maxim <valyavkin99@mail.ru>
2022-06-20 01:58:08 +0300

    20.06

--- js/navigation.js
+++ js/navigation.js
@@ -46,3 +46,40 @@ window.addEventListener('scroll', function (event) {
         navList.style.top = `${navListStyleTop}px`;
     }
 });
+
+//...................................................................................................//
+//Отцентрование на мобилках
+
+const navItems = navList.querySelectorAll('.navigation__item');
+let navItemMaxWidth = 0;
+let currentWidthPosition = 42;
+
+changingWidthNavItems();
+
+addEventListener('resize', () => {
+    if (document.documentElement.clientWidth >= 1050 && currentWidthPosition !== 42) {
+        currentWidthPosition = 0;
+        changingWidthNavItems()
+    } else if (document.documentElement.clientWidth < 1050 && document.documentElement.clientWidth >= 990 && currentWidthPosition !== 1) {
+        currentWidthPosition = 1;
+        changingWidthNavItems()
+    } else if (document.documentElement.clientWidth < 990 && document.documentElement.clientWidth >= 940 && currentWidthPosition !== 2) {
+        currentWidthPosition = 2;
+        changingWidthNavItems()
+    } else if (document.documentElement.clientWidth < 940 && currentWidthPosition !== 3) {
+        currentWidthPosition = 3;
+        changingWidthNavItems()
+    } 
+});
+
+function changingWidthNavItems() {
+    for (item of navItems) {
+        if (item.offsetWidth > navItemMaxWidth) {
+            navItemMaxWidth = item.offsetWidth;
+        }
+    }
+    
+    for (item of navItems) {
+        item.style.width = `${navItemMaxWidth}px`
+    }
+}
